dd22d10d10e7cc32275e75d0b1687a242e040ea2
tarisaka <52767566+tarisaka@users.noreply.github.com>
2023-12-10 14:49:54 +0900

    Update sw.js

--- sw.js
+++ sw.js
@@ -2,3 +2,5 @@ self.addEventListener('message', function (event) {
   console.log("[sw.js] " + event);
   self.registration.showNotification(event.data);
 });
+
+self.addEventListener('fetch', function(event) {});
