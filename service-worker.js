17b4f8b276dd0236b33a3cd3bd90cbced677343e
tarisaka <52767566+tarisaka@users.noreply.github.com>
2023-12-10 15:07:23 +0900

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -7,5 +7,10 @@ self.addEventListener('activate', function(e) {
   console.log('[ServiceWorker] Activate');
 });
 
+self.addEventListener('message', function (event) {
+  console.log("[ServiceWorker][message] " + event.data);
+  self.registration.showNotification(event.data);
+});
+
 // 現状では、この処理を書かないとService Workerが有効と判定されないようです
 self.addEventListener('fetch', function(event) {});
