1b578ab076a4f70a0581926e7a6c818220936c49
tarisaka <52767566+tarisaka@users.noreply.github.com>
2023-12-10 16:09:11 +0900

    Delete sw.js

--- sw.js
+++
@@ -1,6 +0,0 @@
-self.addEventListener('message', function (event) {
-  console.log("[sw.js] " + event);
-  self.registration.showNotification(event.data);
-});
-
-self.addEventListener('fetch', function(event) {});
